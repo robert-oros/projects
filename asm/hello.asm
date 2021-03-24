d61a4e18c8ee3be0d0cef278a3dd610a703ecf3e
Robert Oros <robertstefanoros@gmail.com>
2021-03-24 14:51:31 +0200

    Modify Usage

--- asm/hello.asm
+++ asm/hello.asm
@@ -5,8 +5,8 @@ section .bss
 section .text
     global main
 main:
-    mov    rax, 101       ; 1 = write
-    mov    rdi, 1       ; 1 = to stdout
+    mov    rax, 1       ; 1 = write
+    mov    rdi, 1     ; 1 = to stdout
     syscall             ; display the string
     mov    rax, 60      ; 60 = exit
     mov     rdi, 0      ; 0 = success exit code

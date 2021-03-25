9b6a309182bac3039853e7884164d065f470950c
Robert Oros <robertoros403@gmail.com>
2021-03-25 19:20:38 +0200

    Add

--- sh/rename_files/rename.sh
+++ sh/rename_files/rename.sh
@@ -4,14 +4,17 @@ for i in {9..100};
 do  
     count=$((i-1))
 
-    if [[ $i -lt 10 ]]
+    if [[ $i -le 9 ]]
     then 
         $(mv "00$i ana.mp4" "00$count ana.mp4")
         echo "this file is renamed"
-    elif [[ $i -lt 99 ]]
+    elif [[ $i -le 99 ]]
     then
         $(mv "0$i ana.mp4" "0$count ana.mp4")
         echo "this file is renamed"
+    else
+        $(mv "$i ana.mp4" "$count ana.mp4")
+        echo "this file is renamed"     
     fi
 done
 

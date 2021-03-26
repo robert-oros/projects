69b4fd6dd41af87a4a45afd5357dce449bc63461
Robert Oros <robertoros403@gmail.com>
2021-03-26 19:09:53 +0200

    Modify rename.sh

--- sh/rename_files/rename.sh
+++ sh/rename_files/rename.sh
@@ -1,42 +1,21 @@
 #!/bin/bash
 
 
-j=8
-for i in {1..100};
-do  
-    j=$((j+1))
-    file_name="file.txt" 
-
+i=9-1
+for f in *.txt; do 
+    i=$((i+1))
+    new="file.txt" 
     
-    if [ $i -le 9 ] || [ $j -le 9 ]; then
-        echo "00$i $file_name | 00$j $file_name"
-        # echo "j"
-    #     `mv "00$i $file_name" "00$j $file_name"`
-    #     echo "this file is renamed"
-    elif [[ $j -le 99 ]]; then
-        echo "0$i $file_name | 0$j $file_name"
-        # `mv "0$i $file_name" "0$j $file_name"`
-        # echo "this file is renamed"
-    elif [[ $i -le 100 ]]; then
-        echo "$i $file_name | $j $file_name"
-        # `mv "$i $file_name" "$j $file_name"`
-        #  echo "this file is renamed"     
+    if [[ $i -le 9 ]]; then # i <= 9: then put 00 before 
+        mv "$f" " 00$i$new"
+    elif [[ $i -le 99 ]]; then
+        mv "$f" " 0$i$new"
+    else
+        mv "$f" " 0$i$new"
     fi
+    
 done
 
-
-# i=8
-# for f in *.txt
-# do 
-#     i=$((i+1))
-    
-#     echo "$f $i" 
-# done
-
-# for f in *.mp4
-# do 
-#     echo $f
-# done
 
 # https://linuxhint.com/rename_file_bash/
 # https://www.webservertalk.com/bash-increment-decrement-variable

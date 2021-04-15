670ad5dfe1aa7756f0f2aed4b4255280c5b7a861
Robert Oros <robertstefanoros@gmail.com>
2021-04-15 12:32:09 +0300

    Add dowload.sh

--- sh/dowload.sh
+++ sh/dowload.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
 check_if_exit(){
+    tool=$1
 
+    if [ $(command -v $tool) ]; then
+        echo "exist"
+        remove_tool $tool
+        exit 1
+    else
+        dowload $tool
+    fi
 }
 
-remove_if_exist(){
-
+remove_tool(){
+    rm_tool=$1
+    
+    if [ $(command -v $rm_tool) ]; then
+        echo "exist"
+        sudo apt-get remove $rm_tool
+         
+        dowload $rm_tool
+    fi
 }
 
 dowload(){
-    
+    dowload_tool=$1
+
+    sudo apt-get install $dowload_tool
+    # mkdir /tmp/tmp_dir
+    # cd /tmp/tmp_dir
+    # curl -sS https://github.com/mirror/wget/archive/refs/tags/v1.21.1.zip > file.zip
+    # unzip file.zip -d /home/robert/Desktop/
 }
+
+
+check_if_exit wget 
